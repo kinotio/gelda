e1ec96d4583587a497d2cc0d628aab99c6e68d20
Ando <andostronaut@gmail.com>
2024-06-30 20:02:58 +0300

    fix: install supabase js deps and setup lib on server

--- lib/utils.ts
+++ lib/utils.ts
@@ -14,3 +14,15 @@ export const getInitials = (user: UserInformationType | undefined) => {
     ?.join('')
     .toUpperCase()
 }
+
+export const formatDate = (date: string) => {
+  let d = new Date(date)
+  let day = d.getDate().toString().padStart(2, '0')
+  let month = (d.getMonth() + 1).toString().padStart(2, '0')
+  let year = d.getFullYear().toString().slice(-2)
+  let minutes = d.getMinutes().toString().padStart(2, '0')
+  let hours = d.getHours().toString().padStart(2, '0')
+  let seconds = d.getSeconds().toString().padStart(2, '0')
+
+  return `${day}-${month}-${year} ${minutes}:${hours}:${seconds}`
+}
