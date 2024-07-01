224102fbba876626803fba89dcaccb7c955686c4
Ando <andostronaut@gmail.com>
2024-07-01 14:19:08 +0300

    feat: migrate roles and statuses to be in mod server action

--- server/actions/mod/statuses/queries.ts
+++ server/actions/mod/statuses/queries.ts
@@ -3,7 +3,7 @@
 import { database } from '@/server/config/database'
 import { response } from '@/server/lib/helpers'
 
-export const getAll = async () => {
+export const getAllStatusesQuery = async () => {
   try {
     const data = await database.query.statuses.findMany()
     return response(true, 'Statuses fetched successfully', data)
